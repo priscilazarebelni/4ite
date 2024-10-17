739d43e0bc5de46fa342d3a8bbae1227188c3ce8
priscilazarebelni <160619289+priscilazarebelni@users.noreply.github.com>
2024-10-17 11:32:45 -0300

    Add files via upload

---
+++ pri2anob/perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Ciências',
+    'Qual é o estado fíico da água a 0°C?',
+    'Sólido (gelo)'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro a Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em território?',
+    'Rússia'
+)
+
+criaCartao(
+    '',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
